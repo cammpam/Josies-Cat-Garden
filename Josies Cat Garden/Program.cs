b467567202752567fdcb2874691b169d05e1935e
Cameron Pam <cammpam.creative@gmail.com>
2017-09-21 11:15:57 -0700

    Add project files.

---
+++ Josies Cat Garden/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Josies_Cat_Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.Write("Catnip ");
+                    }
+
+                    else
+                    {
+                        Console.Write("Mint ");
+                    }
+                }
+                Console.WriteLine();
+
+            }
+            Console.Read();
+        }
+    }
+}
